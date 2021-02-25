636d83a2820dbd4b027ac60929f99d712db2146c
Jarad Gray <jarad.h.gray@gmail.com>
2021-02-25 15:41:49 -0600

    add app icon image to MainWindow

--- src/DynamicWallpaper/Properties/Resources.Designer.cs
+++ src/DynamicWallpaper/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace DynamicWallpaperNamespace.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap AppIcon_256 {
+            get {
+                object obj = ResourceManager.GetObject("AppIcon_256", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
